bfa30bef1d3d2c29a825ef87cbbf096964e19066
Antoine Rey <82040673+Antoine2803@users.noreply.github.com>
2026-02-04 15:48:52 +0100

    feat: complementary information on process that open files
        Co-authored-by: Antoine Lebeault <antoileb@users.noreply.github.com>

--- src/lsm.c
+++ src/lsm.c
@@ -4,6 +4,7 @@
 #include <uapi/linux/lsm.h>
 #include <linux/module.h>
 #include <linux/dcache.h>
+#include <linux/delay.h>
 
 #include "tlsm.h"
 
@@ -16,18 +17,55 @@ static int count = 0;
 static int log_open(struct file *f)
 {
 	kuid_t uid;
-	int buflen = 256;
+	const int buflen = 256;
 	char buf[buflen];
-	count++;
+	char comm[TASK_COMM_LEN];
+	char exe_buf[256];
+	char *exe_path = "unknown";
+
+	struct task_struct *curr = get_current();
 
 	uid = current_uid();
 	struct inode *inode = file_inode(f);
 	char *res = d_path(&f->f_path, buf, buflen);
 
+	get_task_comm(comm, curr);
+
+	struct file *exe_file = get_task_exe_file(curr);
+	if (exe_file)
+	{
+		char *tmp = d_path(&exe_file->f_path, exe_buf, sizeof(exe_buf));
+		if (!IS_ERR(tmp))
+			exe_path = tmp;
+		fput(exe_file);
+	}
+
 	if (IS_ERR(res))
 		res = "unknown";
 
-	printk(KERN_DEBUG "[TLSM] (%d) - %d is trying to access to file=%s (inode=%ld)", count, __kuid_val(uid), res, inode->i_ino);
+	if (strstr(res, "/home/") != NULL)
+	{
+		count++;
+		printk(KERN_DEBUG
+			   "[TLSM] (%d) pid=%d uid=%d comm=%s exe=%s accessing %s (inode=%ld)\n",
+			   count,
+			   task_tgid_nr(current),
+			   __kuid_val(uid),
+			   comm,
+			   exe_path,
+			   res,
+			   inode->i_ino);
+
+		if ((strstr(comm, "cat") != NULL) && (strstr(res, "test.txt") != NULL))
+		{
+			ssleep(5);
+			return 0;
+		}
+		if ((strstr(comm, "ls") != NULL) && (strstr(res, "image") != NULL))
+		{
+			return 1;
+		}
+	}
 
 	return 0;
 }
