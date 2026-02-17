757e0ef71c9add51732429f81c1918c1b79076c3
antoileb <235707767+antoileb@users.noreply.github.com>
2026-02-17 14:44:03 +0100

    chore(tlsmd): static binary with pyinstaller

--- tools/build.sh
+++ tools/build.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/bash
 
+rm -rf build dist
+echo -e "building static tlsmd with pyinstall using venv. Setup your venv if this fails !"
+
+if [ ! -d "../venv/bin/activate" ]; then
+    
+    python -m venv ../venv
+    source ../venv/bin/activate
+    pip install -r requirements.txt 
+else
+    source ../venv/bin/activate
+fi
+
+pyinstaller --collect-all "six" -F src/tlsm-daemon.py
+
 rm -rf build
 mkdir build
-tar -czf ./build/tools-src.tar.gz src
+
+tar -czf ./build/tools-src.tar.gz src dist
 
 cp PKGBUILD build/
 pushd build || exit 
+
 PKGDEST="../../" makepkg -sf
 popd || exit
+
+rm -rf dist
 rm -rf build
-
-
